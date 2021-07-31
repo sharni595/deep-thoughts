767d2b93d520afa48436cb64d3e0bbb3fca76b63
Sharni Zaugg <sharni.greener@gmail.com>
2021-07-31 17:48:02 -0600

    add thought form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,8 @@ import ThoughtList from '../components/ThoughtList';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 
+import ThoughtForm from '../components/ThoughtForm';
+
 const Home = () => {
   //use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -19,6 +21,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading . . .</div>
